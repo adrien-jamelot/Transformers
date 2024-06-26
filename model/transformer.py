04795473ef7dac5109c1ddf9a95e572542117eb3
jamadri <adrien.jamelot@gmail.com>
2024-06-26 22:33:52 +0200

    moved config to separate file, refined classes

--- model/transformer.py
+++ model/transformer.py
@@ -2,50 +2,6 @@ from torch import nn
 import torch
 
 
-model_parameters_default = {
-    "batch_size": 512,
-    "encoder": {
-        "nb_layers": 6,
-        "blocks": {
-            "multihead": {
-                    "attention": {
-                        "dim_model": 512,
-                        "dim_key": 64,
-                        "dim_value": 64
-                    },
-                    "nb_head": 8
-                },
-            "feedforward": {
-                    "dim_input": ?,
-                    "dim_output": ?
-                }
-            }
-    },
-    "decoder": {
-        "layers": {
-            "1": {
-                "attention": {
-                    "masked": "true",
-                    "dim_model": 512,
-                    "dim_key": 64,
-                    "dim_value": 64
-                },
-                "nb_head": 8
-            },
-            "2": {
-                "attention": {
-                    "masked": "false",
-                    "dim_model": 512,
-                    "dim_key": 64,
-                    "dim_value": 64,
-                },
-                "nb_head": 8
-            }
-        }
-    }
-}
-
-
 class Transformer(nn.Module, model_parameters=model_parameters_default):
     """Main transformer block.
 
@@ -76,42 +32,45 @@ class Transformer(nn.Module, model_parameters=model_parameters_default):
         return h
 
 
-class Encoder(nn.Module, encoderConfig):
+class Encoder(nn.Module):
     """Encoder.
     """
     def __init__(self, encoderConfig):
         super().__init__()
-        self.update = nn.Sequential(
-            [AddAndNorm(MultiHeadAttention(encoderConfig["multihead"])),
-             AddAndNorm(nn.Linear()),
-             for i in range(encoderConfig["nbLayer"])]
-        )
-            
+        self.dim_model = encoderConfig["dim_model"]
+        self.update = nn.Sequential(OrderedDict([
+            ('multihead'+i%2, AddAndNorm(self.dim_model,
+                        MultiHeadAttention(encoderConfig["multihead"]))),
+            ('feedforward'+i%2, AddAndNorm(self.dim_model,
+                                           nn.Linear()))
+        ]))
+
     def forward(self, x):
         return self.update(x)
 
-        
-class AddAndNorm(nn.Module, block):
-    """Residual connection.
-    """
+
+class AddAndNorm(nn.Module, dim_model, block):
+    """Residual connection."""
     def __init__(self):
         super().__init__()
-        self.norm = ?
+        self.norm = nn.LayerNorm(normalized_shape = dim_model,
+                                 elementwise_affine=True,
+                                 bias=True)
     def forward(self, x):
         return self.norm(x + block(x))
 
-    
+
 class ScaledDotProductAttention(nn.Module, dim_model, attention_parameters):
     """Scaled Dot-Product Attention.
 
     Inputs:
     - dim_model: number of input channels that the model treats at once.
     - attention_parameters: dictionary of key-values describing the attention
-      dimensions as:
-      - dim_key: dimension of the key and query
-      - dim_value: dimension of the value.
+    dimensions as:
+    - dim_key: dimension of the key and query
+    - dim_value: dimension of the value.
 
-    """
+"""
 
     def __init__(self, dim_model, attention_parameters):
         """Initialize parameters."""
@@ -127,38 +86,38 @@ class ScaledDotProductAttention(nn.Module, dim_model, attention_parameters):
             self.activation,
         )
 
-    def forward(self, x):
-        """Forward Pass."""
-        sdpa = torch.matmul(x, self.K)
-        sdpa = mask(sdpa)
-        sdpa = self.activation(sdpa)
-        sdpa = torch.matmul(sdpa, self.V)
+        def forward(self, x):
+            """Forward Pass."""
+            sdpa = torch.matmul(x, self.K)
+            sdpa = mask(sdpa)
+            sdpa = self.activation(sdpa)
+            sdpa = torch.matmul(sdpa, self.V)
 
 
-class MultiHeadAttention(nn.Module, multi_head_config):
-    """Multi-Head Attention.
+    class MultiHeadAttention(nn.Module, multi_head_config):
+        """Multi-Head Attention.
 
-    Inputs:
-    - multi_head_config: dictionary
-    """
+        Inputs:
+        - multi_head_config: dictionary
+        """
 
-    def __init__(self, multi_head_config):
-        """Initialize multi-head."""
-        super().__init__()
-        self.dim_key = multi_head_config["layer"]["dim_key"]
-        self.dim_model = multi_head_config["layer"]["dim_model"]
-        self.nb_head = multi_head_config["nb_head"]
-        self.heads = [
-            nn.Sequential(
-                nn.Linear(self.dim_key, self.dim_model),
-                ScaledDotProductAttention(
-                    dim_model=self.dim_model,
-                    attention_parameters=multi_head_config["layer"]
+        def __init__(self, multi_head_config):
+            """Initialize multi-head."""
+            super().__init__()
+            self.dim_key = multi_head_config["layer"]["dim_key"]
+            self.dim_model = multi_head_config["layer"]["dim_model"]
+            self.nb_head = multi_head_config["nb_head"]
+            self.heads = [
+                nn.Sequential(
+                    nn.Linear(self.dim_key, self.dim_model),
+                    ScaledDotProductAttention(
+                        dim_model=self.dim_model,
+                        attention_parameters=multi_head_config["layer"]
+                    )
                 )
-            )
-            for i in range(self.nb_head)
-        ]
+                for i in range(self.nb_head)
+            ]
 
-    def forward(self, x):
-        """One step of the multi-head block."""
-        return torch.cat([head(x) for head in self.heads], 0)
+        def forward(self, x):
+            """One step of the multi-head block."""
+            return torch.cat([head(x) for head in self.heads], 0)
