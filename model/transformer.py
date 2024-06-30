fd07c43a5654afcc83c59bbc5f9f3a4cca23e806
jamadri <adrien.jamelot@gmail.com>
2024-06-30 21:04:47 +0200

    More litterature. Un-factored a lot to make the architecture codable.

--- model/transformer.py
+++ model/transformer.py
@@ -27,117 +27,88 @@ class Transformer(nn.Module, model_parameters=model_parameters_default):
     def forward(self, x):
         """Apply a step forward."""
         for layer in self.config["encoder"]:
-            
             h = MultiHeadAttention(layer["nb_head"])
         return h
 
 
-
-class AddAndNorm(nn.Module, dim_model, block):
+def addAndNorm(x, block, norm):
     """Residual connection."""
-    def __init__(self, dim_model, block):
-        super().__init__()
-        self.norm = nn.LayerNorm(normalized_shape = dim_model,
-                                 elementwise_affine=True,
-                                 bias=True)
-        self.block = block
-    def forward(self, x):
-        return self.norm(x + self.block(x))
+    return norm(x + block(x))
 
 
 class Encoder(nn.Module):
-    """Encoder.
-    """
+    """Encoder."""
 
     def __init__(self, encoderConfig):
+        """Initialize."""
         super().__init__()
         self.dim_model = encoderConfig["dim_model"]
-        self.layer = []
-        for i in range(encoderConfig["nb_layers"]):
-            self.layer.append(
-                ('multihead' + str(i),
-                AddAndNorm(self.dim_model,
-                           MultiHeadAttention(encoderConfig["multihead"])
-                           )
-                 )
-            )
-            self.layer.append(
-                ('feedforward' + str(i),
-                AddAndNorm(self.dim_model,
-                           nn.Linear(self.dim_model, self.dim_model)
-                           )
-                 )
-            )
-        self.update = nn.Sequential(
-            *self.layer
-        )
+        self.norm = nn.LayerNorm(normalized_shape=self.dim_model,
+                                 elementwise_affine=True, bias=True)
+        self.multiheads = [MultiHeadAttention(encoderConfig["multihead"])
+                           for i in range(self.nb_layers)]
+        self.feedforwards = [nn.Linear(self.dim_model, self.dim_model)
+                             for i in range(self.nb_layers)]
 
     def forward(self, x):
-        return self.update(x)
+        """Forward."""
+        for i in range(self.nb_layers):
+            h1 = AddAndNorm(x, self.multiheads[i](x, x, x))
+            x = AddAndNorm(h1, self.feedforwards[i](h1))
+        return x
 
 
 class Decoder(nn.Module):
-    """Decoder.
-    """
-
-    def __init__(self, encoderConfig):
+    """Decoder."""
+    def __init__(self, decoderConfig):
+        """Initialize."""
         super().__init__()
-        self.dim_model = encoderConfig["dim_model"]
+        self.dim_model = decoderConfig["dim_model"]
         self.layer = []
-        for i in range(encoderConfig["nb_layers"]):
-            self.layer.append(
-                ('multihead' + str(i),
-                 AddAndNorm(self.dim_model,
-                            MultiHeadAttention(encoderConfig["multihead"])
-                            )
-                 )
-            )
-            self.layer.append(
-                ('feedforward' + str(i),
-                 AddAndNorm(self.dim_model,
-                            nn.Linear(self.dim_model, self.dim_model)
-                            )
-                 )
-            )
-        self.update = nn.Sequential(
-            *self.layer
+        self.multiheads1 = [MultiHeadAttention(decoderConfig["multihead"])
+                            for i in range(self.nb_layers)]
+        self.multiheads2 = [MultiHeadAttention(decoderConfig["multihead"])
+                            for i in range(self.nb_layers)]
+        self.feedforwards = [nn.Linear(self.dim_model, self.dim_model)
+                             for i in range(self.nb_layers)]
+        self.encoder = Encoder()
+        self.toProba = nn.Sequential(
+            nn.Linear(self.nb_channels, self.dim_model),
+            nn.Softmax()
         )
 
-    def forward(self, x):
-        return self.update(x)
+    def forward(self, input, lastOutput):
+        """Forward."""
+        encoderOutput = self.encoder(input)
+        for i in range(self.nb_layers):
+            h1 = self.AddAndNorm(lastOutput,
+                                 self.multiheads1[i](lastOutput,
+                                                     lastOutput,
+                                                     lastOutput))
+            h2 = self.AddAndNorm(h1,
+                                 self.multiheads2[i](h1,
+                                                     encoderOutput))
+            lastOutput = addAndNorm(h2, self.feedforwards[i](h2))
+        return self.toProba(lastOutput)
 
-class ScaledDotProductAttention(nn.Module, dim_model, attention_parameters):
+
+def ScaledDotProductAttention(Q, K, V, attention_parameters):
     """Scaled Dot-Product Attention.
 
     Inputs:
-    - dim_model: number of input channels that the model treats at once.
-    - attention_parameters: dictionary of key-values describing the attention
-    dimensions as:
-    - dim_key: dimension of the key and query
-    - dim_value: dimension of the value.
-
+    - Q: query
+    - K: key
+    - V: value
     """
+    dim_model = attention_parameters["dim_model"]
+    return torch.matmul(torch.divide(torch.matmul(Q, K),
+                                     torch.sqrt(dim_model)),
+                        V)
 
-    def __init__(self, dim_model, attention_parameters):
-        """Initialize parameters."""
-        super().__init__()
-        self.dim_key = attention_parameters["dim_key"]
-        self.dim_value = attention_parameters["dim_value"]
-        self.K = nn.Linear(self.dim_key, dim_model)  # TODO Check dimensions
-        self.V = nn.Linear(self.dim_value, dim_model)  # TODO
-        self.activation = nn.Softmax()
-        self.sqrt_dim_model = torch.sqrt(dim_model)
-
-        def forward(self, x):
-            """Forward Pass."""
-            sdpa = torch.divide(self.K(x), self.sqrt_dim_model)
-            # sdpa = mask(sdpa)
-            sdpa = self.activation(sdpa)
-            return self.V(sdpa)
 
 class MultiHeadAttention(nn.Module, multi_head_config):
     """Multi-Head Attention.
-    
+
     Inputs:
     - multi_head_config: dictionary
     """
@@ -147,18 +118,20 @@ class MultiHeadAttention(nn.Module, multi_head_config):
         super().__init__()
         self.dim_key = multi_head_config["layer"]["dim_key"]
         self.dim_model = multi_head_config["layer"]["dim_model"]
-        self.nb_head = multi_head_config["nb_head"]
-        self.heads = [
-            nn.Sequential(
-                nn.Linear(self.dim_key, self.dim_model),
-                ScaledDotProductAttention(
-                    dim_model=self.dim_model,
-                    attention_parameters=multi_head_config["layer"]
-                )
-            )
-            for i in range(self.nb_head)
-        ]
-        
-    def forward(self, x):
+        self.nb_heads = multi_head_config["nb_heads"]
+
+        self.WQs = [nn.Linear(self.dim_model, self.dim_key)
+                    for i in range(self.nb_head)]
+        self.WKs = [nn.Linear(self.dim_model, self.dim_key)
+                    for i in range(self.nb_head)]
+        self.WVs = [nn.Linear(self.dim_model, self.dim_value)
+                    for i in range(self.nb_head)]
+
+    def forward(self, Q, K, V):
         """One step of the multi-head block."""
-        return torch.cat([head(x) for head in self.heads], 0)
+        heads = [ScaledDotProductAttention(self.WQs[i](Q),
+                                           self.WKs[i](K),
+                                           self.WVs[i](V),
+                                           self.dim_model)
+                 for i in range(self.nb_heads)]
+        return torch.cat([head for head in heads], 0)
